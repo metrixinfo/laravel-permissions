51a4402fcb169c00aa2a4d3d80194bcf1e0ad467
Michael J Love <mjlovefl@gmail.com>
2021-06-20 05:52:32 -0400

    HF2 Added VERSION and CHANGES files, Version bump to v0.1.0

--- config/config.php
+++ config/config.php
@@ -1,8 +1,15 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Cache TTL
+    |--------------------------------------------------------------------------
+    |
+    | The amount of time a user's permissions are stored in the cache in seconds.
+    | The default value is 1 day (86400)
+    */
+
+    'cache_ttl' => 86400
 ];
